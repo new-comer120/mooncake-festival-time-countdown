aae5326ce720f139b8f3669f8fc298865e4b82be
Nam <toymyorange@gmail.com>
2021-09-19 21:04:48 +0700

    Late Update

---
+++ app.js
@@ -0,0 +1,17 @@
+const countdown = () => {
+	const countDate = new Date("September 21, 2021 20:00:00").getTime();
+	const now = new Date().getTime();
+	const gap = countDate - now;
+
+	const textDay = Math.floor(gap / (1000 * 60 * 60 * 24));
+	const textHour = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	const textMinute = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
+	const textSecond = Math.floor((gap % (1000 * 60)) / 1000);
+
+	document.querySelector(".day").innerText = textDay;
+	document.querySelector(".hour").innerText = textHour;
+	document.querySelector(".minute").innerText = textMinute;
+	document.querySelector(".second").innerText = textSecond;	
+};
+
+setInterval(countdown, 1000);
