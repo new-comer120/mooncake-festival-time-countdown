a0fd3497bb38bcc68aa5345a662d04e18fd5c97a
Nam <toymyorange@gmail.com>
2021-09-21 20:04:20 +0700

    2022 Mooncake Festival Support

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-	const countDate = new Date("September 21, 2021 20:00:00").getTime();
+	const countDate = new Date("September 10, 2022 20:00:00").getTime();
 	const now = new Date().getTime();
 	const gap = countDate - now;
 
